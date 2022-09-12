1e6dcda3e5fe4cdffd54b1a843cc31de83c6977d
Zachattack221 <zsvd09@gmail.com>
2022-09-12 17:56:20 -0400

    working on logic for classes/prompt sequence

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const fs = require('fs');
+const inquirer = require('inquirer'); 
+
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const questions = require('./lib/questions');
+const generateHtml = require('./src/page-template')
 
-// TODO: CODE GOES HERE
+const prompt = inquirer.createPromptModule();
+
+prompt(questions).then((team) => {
+    const template = generateHtml(team);
+    if (!fs.existsSync('dist')) {
+        fs.mkdirSync('dist');
+    }
+          fs.writeFileSync('dist/team.html', template);
+    });
