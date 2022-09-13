803706bd9ee134f7c974e649dc2b61331f04bc49
Zachattack221 <zsvd09@gmail.com>
2022-09-12 23:44:06 -0400

    working on logic to direct user to chain of prompts

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const fs = require('fs');
-// const inquirer = require('inquirer');
-const Employee = require('./lib/Employee'); 
+const inquirer = require('inquirer');
+// const Employee = require('./lib/Employee'); 
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 // const questions = require('./lib/questions');
 const generateHtml = require('./src/page-template');
+const questions = require('./lib/questions');
+
+// create start() to initialize prompts
+
 
 const prompt = inquirer.createPromptModule();
 
-prompt(Employee).then((team) => {
-    const template = generateHtml(team);
-    if (!fs.existsSync('dist')) {
-        fs.mkdirSync('dist');
-    }
-          fs.writeFileSync('dist/team.html', template);
-    });
+// prompt(questions).then((answers) => {
+//         const template = generateHtml(answers);
+//          console.log('testing');
+// });
+
+// prompt(coWorker).then((answers) => {
+//     const template = generateHtml(answers);
+//     if (!fs.existsSync('dist')) {
+//         fs.mkdirSync('dist');
+//     }
+//           fs.writeFileSync('dist/team.html', template);
+//     });
+
