9c6cecbf5926ecb01e3e061b43a3ca9d9d1d8c8c
Zachattack221 <zsvd09@gmail.com>
2022-09-12 22:20:27 -0400

    working on removing questions.js to have questions generate dynamically instead

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
-const inquirer = require('inquirer'); 
+// const inquirer = require('inquirer');
+const Employee = require('./lib/Employee'); 
 
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const questions = require('./lib/questions');
-const generateHtml = require('./src/page-template')
+// const questions = require('./lib/questions');
+const generateHtml = require('./src/page-template');
 
 const prompt = inquirer.createPromptModule();
 
-prompt(questions).then((team) => {
+prompt(Employee).then((team) => {
     const template = generateHtml(team);
     if (!fs.existsSync('dist')) {
         fs.mkdirSync('dist');
